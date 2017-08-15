1b290ab552b2660c08a68630155ffce1561bfb62
David Dassau <todieahero@hotmail.com>
2017-08-15 17:22:39 -0500

    finished inclass project

--- main.js
+++ main.js
@@ -28,15 +28,16 @@ var planets = [{
 var planetHolderDiv = document.getElementById('planetHolder');
 // Variable to grab the div with the showButton id
 var getPlanetsButton = document.getElementById('showButton');
+var inputField = document.getElementById('searchText');
 
 // Function to write the content that will appear on the page
-function domString() {
+function domString(planetz) {
 	var planetString = '';
-	for(var i = 0; i < planets.length; i++){
+	for(var i = 0; i < planetz.length; i++){
 		var newPlanet = '';
 		newPlanet += `<div class="planetBox" id="planetBox-${i}">`;
-		newPlanet += `<div class="planetName">${planets[i].name}</div>`;
-		newPlanet += `<img class="planetImage" src="${planets[i].url}">`;
+		newPlanet += `<div class="planetName hidden">${planetz[i].name}</div>`;
+		newPlanet += `<img class="planetImage" src="${planetz[i].url}">`;
 		newPlanet += `</div>`;
 		planetString += newPlanet;
 	}
@@ -50,8 +51,47 @@ function writeToDom(strang){
 }
 
 getPlanetsButton.addEventListener('mouseenter', function(){
-	domString();
+	domString(planets);
 });
+
+function showMe(e) {
+	e.target.previousSibling.classList.remove('hidden');
+
+}
+
+document.body.addEventListener('click', function(event){
+	// console.log("click event", event);
+	// console.log("click event", event.target.parentNode.parentNode.parentNode);
+	if(event.target.className === 'planetImage') {
+		console.log("yaaaaaaaa!");
+		showMe(event);
+	}
+});
+
+inputField.addEventListener('keypress', function(event){
+	if(event.key === 'Enter') {
+		var txt = inputField.value;
+		// 1. filter planets array
+		var results = planets.filter(function(thing){
+			console.log("filter thing", thing);
+			return thing.name.indexOf(txt)>-1;
+		})
+		// 2. rerun domstring
+		domString(results);
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
